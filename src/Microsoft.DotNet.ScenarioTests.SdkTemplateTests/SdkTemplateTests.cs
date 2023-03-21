4fc0320b9ce28819d579a571d75eb88fa7353825
Matt Mitchell <mmitche@microsoft.com>
2023-03-21 11:31:41 -0700

    Initial scenario test harness check-in This repo enables basic SDK scenario testing for the .NET SDK.
    The harness is modeled off of runtime's single file test runner that self-discovers tests within its own assembly. An executable is generated for each of the test projects, which should correspond to a local grouping of tests. While the harness may be run as a framework dependent app, in many cases it may be more useful to publish as self-contained using an LKG SDK to avoid using the sdk/runtime being tested to run the harness.
    
    Initially, a subset of the smoke tests are included. Tests can be marked with the trait "Category=Offline" to indicate tests that should pass in offline mode.
    
    This code should be offline source-buildable after xunit is included in source-build-externals.

---
+++ src/Microsoft.DotNet.ScenarioTests.SdkTemplateTests/SdkTemplateTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.DotNet.ScenarioTests.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.DotNet.ScenarioTests.SdkTemplateTests;
+
+public class SdkTemplateTests : IClassFixture<ScenarioTestFixture>
+{
+    ScenarioTestFixture _scenarioTestInput;
+    ITestOutputHelper _testOutputHelper;
+    DotNetSdkHelper _sdkHelper;
+
+    public SdkTemplateTests(ScenarioTestFixture testInput, ITestOutputHelper outputHelper)
+    {
+        if (string.IsNullOrEmpty(testInput.DotNetRoot))
+        {
+            throw new ArgumentException("sdk root must be set for sdk tests");
+        }
+
+        _scenarioTestInput = testInput;
+        _testOutputHelper = outputHelper;
+        _sdkHelper = new DotNetSdkHelper(outputHelper, _scenarioTestInput.DotNetRoot, _scenarioTestInput.SdkVersion);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetLanguages))]
+    [Trait("Category", "Offline")]
+    public void VerifyConsoleTemplate(DotNetLanguage language)
+    {
+        var newTest = new SdkTemplateTest(
+            nameof(SdkTemplateTests), language, _scenarioTestInput.TargetRid, DotNetSdkTemplate.Console,
+            DotNetSdkActions.Build | DotNetSdkActions.Run | DotNetSdkActions.PublishComplex | DotNetSdkActions.PublishR2R);
+        newTest.Execute(_sdkHelper, _scenarioTestInput.TestRoot);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetLanguages))]
+    public void VerifyClasslibTemplate(DotNetLanguage language)
+    {
+        var newTest = new SdkTemplateTest(
+            nameof(SdkTemplateTests), language, _scenarioTestInput.TargetRid, DotNetSdkTemplate.ClassLib,
+            DotNetSdkActions.Build | DotNetSdkActions.Publish);
+        newTest.Execute(_sdkHelper, _scenarioTestInput.TestRoot);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetLanguages))]
+    public void VerifyXUnitTemplate(DotNetLanguage language)
+    {
+        var newTest = new SdkTemplateTest(
+            nameof(SdkTemplateTests), language, _scenarioTestInput.TargetRid, DotNetSdkTemplate.XUnit,
+            DotNetSdkActions.Test);
+        newTest.Execute(_sdkHelper, _scenarioTestInput.TestRoot);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetLanguages))]
+    public void VerifyNUnitTemplate(DotNetLanguage language)
+    {
+        var newTest = new SdkTemplateTest(
+            nameof(SdkTemplateTests), language, _scenarioTestInput.TargetRid, DotNetSdkTemplate.NUnit,
+            DotNetSdkActions.Test);
+        newTest.Execute(_sdkHelper, _scenarioTestInput.TestRoot);
+    }
+
+    [Theory]
+    [MemberData(nameof(GetLanguages))]
+    public void VerifyMSTestTemplate(DotNetLanguage language)
+    {
+        var newTest = new SdkTemplateTest(
+            nameof(SdkTemplateTests), language, _scenarioTestInput.TargetRid, DotNetSdkTemplate.MSTest,
+            DotNetSdkActions.Test);
+        newTest.Execute(_sdkHelper, _scenarioTestInput.TestRoot);
+    }
+
+    private static IEnumerable<object[]> GetLanguages() => Enum.GetValues<DotNetLanguage>().Select(lang => new object[] { lang });
+}
